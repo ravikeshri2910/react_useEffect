b78d64d566a4fbb6507c639b5c7a05ee8194bb92
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-11-18 00:38:31 +0530

    contextApi hook

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../store/auth-context';
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext)
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
           <nav className={classes.nav}>
             <ul>
               {ctx.isLoggedIn && (
@@ -22,16 +20,12 @@ const Navigation = (props) => {
               )}
               {ctx.isLoggedIn && (
                 <li>
-                  <button onClick={props.onLogout}>Logout</button>
+                  <button onClick={ctx.onLogout}>Logout</button>
                 </li>
               )}
             </ul>
           </nav>
         )
-      }}
-
-    </AuthContext.Consumer>
-  );
 };
 
 export default Navigation;
