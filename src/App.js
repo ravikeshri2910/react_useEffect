a837dde28b1cd10361634566d945cf14868095d7
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-11-18 00:19:20 +0530

    contextApi basic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import AuthContext from './store/auth-context'
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
     if (loggedInInformation) {
       setIsLoggedIn(true)
     }
-  },[])
+  }, [])
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
@@ -29,13 +30,16 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider value={{
+      isLoggedIn : isLoggedIn
+    }}
+    >
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
